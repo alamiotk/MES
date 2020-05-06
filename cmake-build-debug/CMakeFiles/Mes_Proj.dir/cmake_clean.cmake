f5389eec8b3255cbc8a0b25123d7dbc8e688702c
alamiotk <ala.miotk@gmail.com>
2020-05-06 21:26:37 +0200

    Init commit

---
+++ cmake-build-debug/CMakeFiles/Mes_Proj.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Mes_Proj.dir/main.cpp.o"
+  "Mes_Proj.pdb"
+  "Mes_Proj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Mes_Proj.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
